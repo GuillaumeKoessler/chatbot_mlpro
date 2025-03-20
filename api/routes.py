317d6b77f96eb74a1a1e0347e86df0ee2c3c4896
GuillaumeKoessler <guillaume.koessler@gmail.com>
2025-03-20 23:26:18 +0100

    add conversation to api

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
 
 from langchain_core.messages import HumanMessage
 
@@ -17,6 +18,7 @@ app = FastAPI()
 
 chain = None
 
+
 @app.get("/")
 async def root():
     print("Hello world")
@@ -33,6 +35,7 @@ async def alexandrin(file: UploadFile = File(...)):
 
     return {"vers": vers}
 
+
 @app.post("/init_conv")
 async def init_conv(file: UploadFile = File(...)):
     global chain
@@ -41,29 +44,36 @@ async def init_conv(file: UploadFile = File(...)):
     content = await file.read()
     text = content.decode("utf-8")
 
-    chain = StateGraph(State)
+    poem = transf_phrase_vers(text)
 
-    chain.add_node(
-        "conversation", lambda input: call_model(state=input, conversation_summary=text)
+    workflow = StateGraph(State)
+    workflow.add_node(
+        "conversation", lambda input: call_model(state=input, conversation_summary=poem)
     )
+    workflow.add_node(summarize_history)
+    workflow.add_edge(START, "conversation")
+    workflow.add_conditional_edges(
+        "conversation",
+        should_continue,
+    )
+    workflow.add_edge("summarize_history", END)
+    memory = MemorySaver()
+    chain = workflow.compile(checkpointer=memory)
+
+    return {"poème": poem}
 
 
-workflow.add_node(summarize_history)
+async def generate_stream(input_message, config, chain):
+    for event in chain.stream(
+        {"messages": [input_message]}, config, stream_mode="updates"
+    ):
+        yield event["conversation"]["messages"][0].content
 
-workflow.add_edge(START, "conversation")
 
-workflow.add_conditional_edges(
-    "conversation",
-    should_continue,
-)
-
-workflow.add_edge("summarize_history", END)
-
-memory = MemorySaver()
-app = workflow.compile(checkpointer=memory)
-
-config = {"configurable": {"thread_id": "4"}}
-input_message = HumanMessage(content="Explique moi les métaphores du poème")
-input_message.pretty_print()
-for event in app.stream({"messages": [input_message]}, config, stream_mode="updates"):
-    print_update(event)
+@app.post("/update")
+async def update(request: str):
+    config = {"configurable": {"thread_id": "4"}}
+    input_message = HumanMessage(content=request)
+    return StreamingResponse(
+        generate_stream(input_message, config, chain), media_type="text/plain"
+    )
